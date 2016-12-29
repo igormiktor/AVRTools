e4ca2ff082db61805ba66d25235e1cb436a664c2
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2016-12-29 12:31:32 -0500

    Full re-write of MemUtils.  Changed function names and made the functions accurately perform as described.

--- AVRTools/MemUtils.cpp
+++ AVRTools/MemUtils.cpp
@@ -25,18 +25,16 @@
 #include <stddef.h>
 #include <avr/io.h>
 
-#include "USART0.h"
 
 
-extern unsigned int         __heap_start;
-extern void*                __brkval;
+// avr-libc internal globals used to manage the heap
+
+extern char                 __heap_start;   // Start of the heap
+extern char*                __brkval;       // Top of the heap (if non-zero)
 extern struct __freelist*   __flp;          // The head of the free list structure
 
 
-/*
- * The free list structure as maintained by the
- * avr-libc memory allocation routines.
- */
+// The free list structure as maintained by the avr-libc memory allocation routines
 
 struct __freelist
 {
@@ -45,18 +43,31 @@ struct __freelist
 };
 
 
+// Utility macros
+
+#define STACK           reinterpret_cast<char*>( AVR_STACK_POINTER_REG )
+
+#define HEAP_START      static_cast<char*>( &__heap_start )
+
+#define HEAP_TOP        static_cast<char*>( __brkval )
+
+
+
+
+
+
 namespace
 {
 
-    // Calculates the size of the free list
-    unsigned int freeListSize()
+    // Calculates the amount of free memory on the free list
+    size_t MemUtils::memoryAvailableOnFreeList()
     {
         struct __freelist* current;
-        unsigned int total = 0;
+        size_t total = 0;
 
         for ( current = __flp; current; current = current->nx )
         {
-            total += 2;         // Add two bytes for the memory block's header
+            // Memory in the header (the "size" slot) is not available for allocation, so don't count it
             total += static_cast<int>( current->sz );
         }
 
@@ -66,28 +77,46 @@ namespace
 };
 
 
-unsigned int MemUtils::freeRam()
-{
-    unsigned int free_memory;
 
-    if ( int( __brkval ) == 0 )
+
+
+size_t MemUtils::freeSRAM()
+{
+    size_t freeMemory;
+
+    if ( HEAP_TOP )
     {
-        free_memory = reinterpret_cast<unsigned int>( &free_memory ) - reinterpret_cast<unsigned int>( &__heap_start );
+        // We have a heap allocation, so need to sum space beyond top of heap and the free list space
+        freeMemory = STACK - HEAP_TOP;
+        freeMemory += memoryAvailableOnFreeList();
     }
     else
     {
-        free_memory = reinterpret_cast<unsigned int>( &free_memory ) - reinterpret_cast<unsigned int>( __brkval );
-        free_memory += freeListSize();
-  }
-  return free_memory;
+        // No heap allocations, so it's free memory from heap start all the way to the stack end
+        freeMemory = STACK - HEAP_START;
+    }
+
+    return freeMemory;
 }
 
 
 
-unsigned int MemUtils::freeRamQuickEstimate()
+
+
+size_t MemUtils::freeMemoryBetweenHeapAndStack()
 {
-    // Extra "-2" at the end makes freeRamQuickEstimate() agree 99% of the time with freeRam()
-    unsigned int v;
-    return reinterpret_cast<unsigned int>( &v ) -
-        ( __brkval == 0 ? reinterpret_cast<unsigned int>( &__heap_start ) : reinterpret_cast<unsigned int>( __brkval ) ) - 2;
+    size_t freeMemory;
+
+    if ( HEAP_TOP )
+    {
+        // We have a heap allocation, so measure from the top of heap
+        freeMemory = STACK - HEAP_TOP;
+    }
+    else
+    {
+        // No heap allocations, so measure from the heap start
+        freeMemory = STACK - HEAP_START;
+    }
+
+    return freeMemory;
 }
--- AVRTools/MemUtils.h
+++ AVRTools/MemUtils.h
@@ -36,7 +36,7 @@
 #ifndef MemUtils_h
 #define MemUtils_h
 
-
+#include <stddef.h>
 
 /*!
  * \brief A namespace providing encapsulation for functions that report the available memory in SRAM
@@ -45,27 +45,32 @@
 namespace MemUtils
 {
 
-/*!
- * \brief Get the number of free bytes remaining in SRAM.
- *
- * \returns The number of free bytes remaining in SRAM.
- *
- */
+    /*!
+    * \brief Get the total free memory remaining in SRAM.
+    *
+    * This includes memory on the free list (if the heap is used) as well as memory available between the heap and the stack
+    * (which grow towards each other). If the heap has been used, the function will walk the free list to determine the
+    * total amount of free memory.
+    *
+    * \returns The number of free bytes remaining in SRAM.
+    *
+    */
 
-unsigned int freeRam();
+    size_t freeSRAM();
 
 
 
-/*!
- * \brief Get a quick estimate of the number of free bytes remaining in SRAM.
- *
- * This provides a quicker, but perhaps slightly inaccurate, estimate of the amount of free memory.
- *
- * \returns The number of free bytes remaining in SRAM.
- *
- */
+    /*!
+    * \brief Get the free memory between the heap and the stack.
+    *
+    * This does not include any memory potentially available within the heap on the free list.  It executes quickly, so
+    * this function is useful for checking to make sure the heap and stack aren't in danger of collision.
+    *
+    * \returns The number of free bytes remaining between the top of the heap and the top of the stack.
+    *
+    */
 
-unsigned int freeRamQuickEstimate();
+    size_t freeMemoryBetweenHeapAndStack();
 
 };
 
